8f4d3121f7c04a330d74ea4a631a82cc8d5bc784
Rebekah Britton <brittonrs26@gmail.com>
2018-07-05 00:16:00 -0400

    Saving changes to test files  On branch master  Changes to be committed: 	modified:   test/OneDriveTest.js 	modified:   test/_test_config.js 	modified:   test/files/testFile1.txt

--- test/OneDriveTest.js
+++ test/OneDriveTest.js
@@ -7,12 +7,14 @@ process.env.NODE_ENV = 'test';
 const configLoc = ("./_" + process.env.NODE_ENV + "_config");
 const mocha = require('mocha');
 const chai = require('chai');
+const assert = require('chai').assert;
 const webdriver = require('selenium-webdriver');
 const By = webdriver.By;
 const until = webdriver.until;
 const config = require(configLoc);
 
 const host = config.webURL;
+const documentsLink = config.docsURL;
 const username = config.userID;
 const password = config.password;
 
@@ -22,8 +24,12 @@ const signonNextButton = '//*[@type="submit"]';
 const signonPassword = '//*[@placeholder = "Password"]';
 const signinButton = '//*[@value="Sign in"]';
 const staySignedOn= '//*[@value="Yes"]';
+const oneDriveIcon = '//*[@class="ms-Icon--OneDrive"]';
+const documentsFolderIcon = '//*[@data-automationid = "Documents"]';
 const uploadButton = '//*[@id="id__491"]';
-let driver;
+const fileUploadStatus = '//*[@class = "OperationMonitor-itemDescription"]';
+const emptyFileMessage = 'Sorry, OneDrive can\'t upload empty folders or empty files. Please try again.';
+const fileDoneMessage = 'Done';
 const capabilities = {
     'browserName' : 'chrome',
     'chromeOptions' : {
@@ -31,32 +37,51 @@ const capabilities = {
     }
 };
 
-const userLogin = function(){
-    driver.get(host);
-    let signin = By.xpath(signonUserName);
-    driver.wait(until.elementLocated(signin,20000));
-    driver.wait(until.elementIsVisible(driver.findElement(signin)),20000);
-    driver.wait(until.elementLocated(By.xpath(signonUserName)), 20000).sendKeys(username);
-    driver.wait(until.elementLocated(By.xpath(signonNextButton)), 20000).click();
-    driver.wait(until.elementLocated(By.xpath(signonPassword)), 20000).sendKeys(password);
-    driver.sleep(3000);
-    driver.wait(until.elementLocated(By.xpath(signinButton)), 20000).click();
-    //select "Yes" in window
-    driver.wait(until.elementLocated(By.xpath(staySignedOn)), 20000).click();
-};
-describe('OneDrive Automation', function(){
-    this.timeout(5000);
-    before(function(done){
-        driver = new webdriver.Builder().
-        withCapabilities(capabilities).
-        build();
-        done();
+
+describe("One Drive Automation", function(){
+   this.timeout(50000);
+   var driver;
+   before(function(){
+       driver = new webdriver.Builder().
+       withCapabilities(capabilities).
+       build();
+   });
+   after(function(){
+   //    driver.quit();
+   });
+   it("I open logon page", function(){
+       return driver.get(host);
+   });
+   it("I enter my username", function(){
+       return driver.wait(until.elementLocated(By.xpath(signonUserName)), 20000).sendKeys(username);
+   });
+   it("I hit 'next'", function(){
+       return driver.wait(until.elementLocated(By.xpath(signonNextButton)), 20000).click();
+   });
+   it("I send my password", function(){
+       return driver.wait(until.elementLocated(By.xpath(signonPassword)), 20000).sendKeys(password);
+   });
+    it("I click 'Yes' on the popup", function(){
+        return driver.wait(until.elementLocated(By.xpath(signinButton)), 20000).click();
     });
-    //after.(function() {
-    //    driver.quit();
-    //});
-    it('User with valid credentials can log in', function(done){
-        userLogin();
-        done();
-        });
-    });
+   it("I click 'login' button", function(){
+       return driver.wait(until.elementLocated(By.xpath(staySignedOn)), 20000).click();
+   });
+   it("I click the 'One Drive' Icon", function(){
+       return driver.wait(until.elementLocated(By.xpath(oneDriveIcon)), 20000).click();
+       driver.sleep(5000);
+   });
+   it("I click the 'Documents' folder", function(){
+       return driver.get(documentsLink);
+   });
+   it("I verify I am in the 'Documents' folder", function(){});
+   it("I click the upload button in the documents folder", function(){
+
+   });
+   it("I choose the file to upload to the documents folder", function(){
+
+   });
+   it("I verify the file uploaded successfully to the documents folder", function(){
+
+   });
+});
--- test/_test_config.js
+++ test/_test_config.js
@@ -4,10 +4,9 @@
 var config = {};
 
 config.webURL = 'https://login.microsoftonline.com';
+config.docsURL= 'https://onedrive.live.com/?id=9909A161E31A594%21104&cid=09909A161E31A594';
 config.userID = 'rsbritton26@outlook.com';
 config.password = 'dianne1019';
-
-
 config.csvPath = '../Downloads/2016_August.csv';
 config.emptyTestFilePath = './test/files/emptyFile.txt';
 config.testFilePath = './test/files/testFile1.txt';
